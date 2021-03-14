d7b453c19842aba848cd1a17a7d0eb518a8ac4ac
AnushkaAmte <anushkaamte@gmail.com>
2021-03-14 23:10:56 +0530

    fixed chrome issue

--- sketch.js
+++ sketch.js
@@ -1,50 +1,134 @@
-let model_url='https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/'
+const model_url =
+  'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
 let pitch;
-let audioContext;
 let mic;
-let freq=0;
-const button=  document.getElementById('button');
+let freq = 0;
+let threshold = 1;
+
+let notes = [
+  {
+    note: 'A',
+    freq: 440
+  },
+  {
+    note: 'E',
+    freq: 329.6276
+  },
+  {
+    note: 'C',
+    freq: 261.6256
+  },
+  {
+    note: 'G',
+    freq: 391.9954
+  }
+];
+
 function setup() {
   createCanvas(400, 400);
-  audioContext=getAudioContext();
-  mic= new p5.AudioIn();
+  audioContext = getAudioContext();
+  mic = new p5.AudioIn();
   mic.start(listening);
   
 }
 
-function listening(){
+function listening() {
+  getAudioContext().resume();
   console.log('listening');
-  
-  pitch=ml5.pitchDetection(model_url,
-  audioContext,
-  mic.stream,
-  modelLoaded);
+  pitch = ml5.pitchDetection(model_url, audioContext, mic.stream, modelLoaded);
 }
 
-function touchStarted(){
-getAudioContext().resume()
-}
+function draw() {
+  background(0);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  textSize(32);
+  text(freq.toFixed(2), width / 2, height - 150);
 
-function gotPitch(error,frequency){
-  if(error){
-    console.log(error);
-  }
-  else{
-    if(frequency){
-      freq=frequency;
+  let closestNote = -1;
+  let recordDiff = Infinity;
+  for (let i = 0; i < notes.length; i++) {
+    let diff = freq - notes[i].freq;
+    if (abs(diff) < abs(recordDiff)) {
+      closestNote = notes[i];
+      recordDiff = diff;
     }
   }
-  pitch.getPitch(gotPitch);
+
+  textSize(64);
+  text(closestNote.note, width / 2, height - 50);
+
+  let diff = recordDiff;
+  // let amt = map(diff, -100, 100, 0, 1);
+  // let r = color(255, 0, 0);
+  // let g = color(0, 255, 0);
+  // let col = lerpColor(g, r, amt);
+
+  let alpha = map(abs(diff), 0, 100, 255, 0);
+  rectMode(CENTER);
+  fill(255, alpha);
+  stroke(255);
+  strokeWeight(1);
+  if (abs(diff) < threshold) {
+    fill(0, 255, 0);
+  }
+  rect(200, 100, 200, 50);
+
+  stroke(255);
+  strokeWeight(4);
+  line(200, 0, 200, 200);
+
+  noStroke();
+  fill(255, 0, 0);
+  if (abs(diff) < threshold) {
+    fill(0, 255, 0);
+  }
+  rect(200 + diff / 2, 100, 10, 75);
 }
 
 function modelLoaded() {
-  console.log('Model Loaded!');
+
+  console.log('model loaded');
   pitch.getPitch(gotPitch);
 }
-function draw() {
+
+function gotPitch(error, frequency) {
+  if (error) {
+    console.error(error);
+  } else {
+    //console.log(frequency);
+    if (frequency) {
+      freq = frequency;
+    }
+    
+    pitch.getPitch(gotPitch);
+  }
+}
+
+ /* Plotly.plot(document.querySelector(".wrapper"),[{
+  y:[getData()],
+  type:'line'
+}]);
+
+var cnt=0;
+
+setInterval(function(){
+  if(audstat==1){
+  Plotly.extendTraces(document.querySelector(".wrapper"),{y:[[getData()]]},[0]);
+  cnt++;
+  if(cnt>300){
+    Plotly.relayout(document.querySelector(".wrapper"),{
+      xaxis:{
+        range:[cnt-300,cnt]
+      }
+    });
+  }
+}},15);
+ */
+/*function draw() {
   background(0);
   textAlign(CENTER,CENTER);
   fill(255);
   textSize(64);
   text(freq.toFixed(2),width/2,height/2);
-}
+}*/
